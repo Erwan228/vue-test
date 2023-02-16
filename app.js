68fdba25704ee194dc7291d6c5b774fa0bfe0336
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-02-16 14:29:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const app = Vue.createApp({
     // data, functions
     // template: '<h2>I am the template</h2>'
+    data() {
+        return {
+            title: 'The final template',
+            author: 'Brandon Rogers',
+            Age: 45,
+        }
+    }
 })
 
 app.mount('#app')
